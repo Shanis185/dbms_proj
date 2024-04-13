499db550542f9ec9224183068784acc5f5958e96
haanoon <hanoonmavoor@gmail.com>
2024-04-13 13:44:38 +0530

    addproduct php created

---
+++ add_product.php
@@ -0,0 +1,23 @@
+<?php
+    $product_code = $_POST['product_code'];
+    $product_name = $_POST['product_name'];
+    $mrp = $_POST['mrp'];
+    $rate = $_POST['rate'];
+    $p_rate = $_POST['p_rate'];    
+
+    $conn = new mysqli('127.0.0.1','root','','dbms');
+    if($conn->connect_error)
+    {
+        die('connection Failed : ' . $conn->connect_error)
+    }
+    else
+    {
+        $stmt = $conn->prepare("insert into product(product_code,product_name,mrp,rate,p_rate) values(?,?,?,?,?)");
+
+        $stmt->bind_param('ssddd',$product_code,$product_ name,$mrp,$rate,$p_rate);
+        $stmt->execute();
+        $stmt->close();
+        echo "added successfully...";
+        $conn->close();
+    }
+?>
